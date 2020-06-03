b4ecb603d16e489cc091ae51c8ff604fcfd12af6
MOHAMMED BEN HARRI <benharri0mohammed@gmail.com>
2020-06-03 19:53:25 +0100

    adding more routes

--- api/routes/product.js
+++ api/routes/product.js
@@ -4,10 +4,10 @@ const ProductController = require('../Controller/ProductController');
 
 
 route.get('/products',ProductController.getProducts);
-route.get('/product',ProductController.getProduct);
+route.get('/product/:id',ProductController.getProduct);
 route.post('/product',ProductController.createProduct);
-route.put('/product',ProductController.updateProduct);
-route.delete('/product',ProductController.deleteProduct);
+route.put('/product/:id',ProductController.updateProduct);
+route.delete('/product/:id',ProductController.deleteProduct);
 
 
 module.exports = route;
